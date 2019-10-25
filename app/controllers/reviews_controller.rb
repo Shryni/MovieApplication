2ce8f6fc547c23d690bde96917babf5ac9f0028f
Shryni Kedambadi <shrynikedambadi@pop-os.localdomain>
2019-10-25 13:36:30 +0100

    First commit: adding all the files to the repository

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,51 @@
+class ReviewsController < ApplicationController
+  def index
+    @movie = Movie.find(params[:movie_id])
+    @reviews = @movie.reviews
+  end
+
+  def show
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.build
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.build(params.require(:review).permit(:details))
+    if @review.save
+      redirect_to movie_review_url(@movie, @review)
+    else
+      render :action => "new"
+    end
+  end
+
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    if @review.update_attributes(params.require(:review).permit(:details))
+      redirect_to movie_review_url(@movie, @review)
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    respond_to do |format|
+      format.html { redirect_to movie_reviews_path(@movie) }
+      format.xml { head :ok }
+    end
+  end
+end
